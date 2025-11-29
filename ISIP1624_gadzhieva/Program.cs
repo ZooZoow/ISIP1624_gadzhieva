b992133d8c2c65741d795c72df1e18138b12f0a0
Zyz <zuzz001zu@gmail.com>
2025-11-29 15:36:49 +0300

    практика 7

--- ISIP1624_gadzhieva/Program.cs
+++ ISIP1624_gadzhieva/Program.cs
@@ -1,2 +1,312 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarServiceGame
+{
+    // Перечисление для типов деталей
+    public enum PartType
+    {
+        Engine,
+        Brakes,
+        Transmission,
+        Battery,
+        Tires,
+        Suspension,
+        Exhaust
+    }
+
+    // Класс Деталь
+    public class Part
+    {
+        public PartType Type { get; private set; }
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+        public int Quantity { get; private set; }
+
+        public Part(PartType type, string name, decimal price, int quantity)
+        {
+            Type = type;
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public void IncreaseQuantity(int amount)
+        {
+            Quantity += amount;
+        }
+
+        public void DecreaseQuantity(int amount)
+        {
+            Quantity -= amount;
+        }
+    }
+
+    // Класс Склад
+    public class Warehouse
+    {
+        private Dictionary<PartType, Part> parts;
+
+        public Warehouse()
+        {
+            parts = new Dictionary<PartType, Part>();
+        }
+
+        public void AddPart(Part part)
+        {
+            // TODO: Реализовать добавление детали
+        }
+
+        public bool HasPart(PartType partType)
+        {
+            // TODO: Реализовать проверку наличия детали
+            return false;
+        }
+
+        public Part GetPart(PartType partType)
+        {
+            // TODO: Реализовать получение детали
+            return null;
+        }
+
+        public void RemovePart(PartType partType)
+        {
+            // TODO: Реализовать удаление детали
+        }
+
+        public void DisplayParts()
+        {
+            // TODO: Реализовать отображение деталей на складе
+        }
+    }
+
+    // Класс Машина
+    public class Car
+    {
+        public string Model { get; private set; }
+        public PartType BrokenPart { get; private set; }
+
+        public Car(string model, PartType brokenPart)
+        {
+            Model = model;
+            BrokenPart = brokenPart;
+        }
+    }
+
+    // Класс Клиент
+    public class Client
+    {
+        public string Name { get; private set; }
+        public Car Car { get; private set; }
+
+        public Client(string name, Car car)
+        {
+            Name = name;
+            Car = car;
+        }
+    }
+
+    // Класс Заказ на ремонт
+    public class RepairOrder
+    {
+        public Client Client { get; private set; }
+        public decimal RepairCost { get; private set; }
+        public bool IsCompleted { get; private set; }
+        public bool IsAccepted { get; private set; }
+
+        public RepairOrder(Client client, decimal repairCost)
+        {
+            Client = client;
+            RepairCost = repairCost;
+            IsCompleted = false;
+            IsAccepted = false;
+        }
+
+        public void Accept()
+        {
+            // TODO: Реализовать принятие заказа
+        }
+
+        public void Complete()
+        {
+            // TODO: Реализовать завершение заказа
+        }
+
+        public void Decline()
+        {
+            // TODO: Реализовать отказ от заказа
+        }
+    }
+
+    // Класс Баланс
+    public class Balance
+    {
+        public decimal Amount { get; private set; }
+
+        public Balance(decimal initialAmount)
+        {
+            Amount = initialAmount;
+        }
+
+        public void AddMoney(decimal amount)
+        {
+            // TODO: Реализовать добавление денег
+        }
+
+        public void SubtractMoney(decimal amount)
+        {
+            // TODO: Реализовать вычитание денег
+        }
+
+        public bool CanAfford(decimal amount)
+        {
+            // TODO: Реализовать проверку достаточности средств
+            return false;
+        }
+    }
+
+    // Класс Заказ на покупку
+    public class PurchaseOrder
+    {
+        public PartType PartType { get; private set; }
+        public int Quantity { get; private set; }
+        public int DeliveryTime { get; private set; }
+
+        public PurchaseOrder(PartType partType, int quantity)
+        {
+            PartType = partType;
+            Quantity = quantity;
+            DeliveryTime = 2;
+        }
+
+        public void ProcessDelivery()
+        {
+            // TODO: Реализовать обработку доставки
+        }
+    }
+
+    // Главный класс Автосервис
+    public class CarService
+    {
+        private Balance balance;
+        private Warehouse warehouse;
+        private List<PurchaseOrder> purchaseOrders;
+        private int carsProcessed;
+
+        public CarService(decimal initialBalance)
+        {
+            balance = new Balance(initialBalance);
+            warehouse = new Warehouse();
+            purchaseOrders = new List<PurchaseOrder>();
+            carsProcessed = 0;
+        }
+
+        public void InitializeGame()
+        {
+            // TODO: Реализовать инициализацию игры
+        }
+
+        public void ServeNextClient()
+        {
+            // TODO: Реализовать обслуживание следующего клиента
+        }
+
+        public void DisplayClientInfo(Client client, decimal repairCost)
+        {
+            // TODO: Реализовать отображение информации о клиенте
+        }
+
+        public void AcceptRepair(RepairOrder order)
+        {
+            // TODO: Реализовать принятие ремонта
+        }
+
+        public void DeclineRepair(RepairOrder order)
+        {
+            // TODO: Реализовать отказ от ремонта
+        }
+
+        public void ProcessPurchaseOrders()
+        {
+            // TODO: Реализовать обработку заказов на покупку
+        }
+
+        public void BuyPartsMenu()
+        {
+            // TODO: Реализовать меню покупки деталей
+        }
+
+        public void BuyParts(PartType partType, int quantity)
+        {
+            // TODO: Реализовать покупку деталей
+        }
+
+        public void DisplayStatus()
+        {
+            // TODO: Реализовать отображение статуса
+        }
+
+        public Client GenerateRandomClient()
+        {
+            // TODO: Реализовать генерацию случайного клиента
+            return null;
+        }
+
+        public decimal CalculateRepairCost(PartType partType)
+        {
+            // TODO: Реализовать расчет стоимости ремонта
+            return 0;
+        }
+
+        public bool TryRepairCar(RepairOrder order)
+        {
+            // TODO: Реализовать попытку починки машины
+            return false;
+        }
+
+        public void ProcessWrongRepair(RepairOrder order)
+        {
+            // TODO: Реализовать обработку неправильного ремонта
+        }
+    }
+
+    // Класс для валидации ввода
+    public static class InputValidator
+    {
+        public static bool ValidatePositiveDecimal(string input, out decimal result)
+        {
+            // TODO: Реализовать валидацию положительного decimal
+            result = 0;
+            return false;
+        }
+
+        public static bool ValidatePositiveInt(string input, out int result)
+        {
+            // TODO: Реализовать валидацию положительного int
+            result = 0;
+            return false;
+        }
+
+        public static bool ValidatePartType(string input, out PartType result)
+        {
+            // TODO: Реализовать валидацию типа детали
+            result = PartType.Engine;
+            return false;
+        }
+    }
+
+    // Главная программа
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            CarService game = new CarService(10000m);
+            game.InitializeGame();
+
+            // TODO: Реализовать главный игровой цикл
+        }
+    }
+}
